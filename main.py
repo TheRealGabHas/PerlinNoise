ae0133ce8811959c771b8be8b7e4b6ceab4d96fb
TheRealGabHas <gabhas.gabhas@gmail.com>
2022-12-19 00:04:06 +0100

    Changed the file structure to be object-oriented. Added seeded randomness.

--- main.py
+++ main.py
@@ -8,13 +8,8 @@ Highly inspired by this tutorial : https://rtouti.github.io/graphics/perlin-nois
 """
 
 
-#  Returns a blank canvas following the inputted width and height
-def create_canvas(width: int, height: int) -> Image:
-    return Image.new("RGB", (width, height), "#FFFFFF")
-
-
 #  Returns a flattened (near 0 and 1) version of an inputted value
-def fade(t: float | int) -> int | float:
+def _fade(t: float | int) -> int | float:
     #  Only handle values between 0 and 1
     #  Entering a value < 0.5 will return a value closer to 0
     #  Entering a value > 0.5 will return a value closer to 1
@@ -22,19 +17,8 @@ def fade(t: float | int) -> int | float:
     return ((6 * t - 15) * t + 10) * t * t * t
 
 
-#  Returns a permutation table of 2 times the inputted size
-def perm_table(size: int) -> list[int]:
-    array = [i for i in range(size)]
-    random.shuffle(array)
-
-    for i in range(size):
-        array.append(array[i])
-
-    return array
-
-
 #  Returns a constant vector from an inputted integer
-def get_constant_vect(value: int) -> tuple[int, int]:
+def _get_constant_vect(value: int) -> tuple[int, int]:
     vect = tuple()
     match value % 4:
         case 0:
@@ -49,19 +33,19 @@ def get_constant_vect(value: int) -> tuple[int, int]:
 
 
 # Returns the dot product of 2 vectors of coordinates (x; y)
-def dot_product_2d(vect1: tuple, vect2: tuple) -> float:
+def _dot_product_2d(vect1: tuple, vect2: tuple) -> float:
     return vect1[0] * vect2[0] + vect1[1] * vect2[1]
 
 
 #  Returns a value that lies the 2 inputted ones
-def linear_interpolation(t: float, value1: int | float, value2: int | float) -> float:
+def _linear_interpolation(t: float, value1: int | float, value2: int | float) -> float:
     return value1 + t * (value2 - value1)
 
 
 #  Returns a float value between -1 and 1 for an inputted point of coordinates (x; y)
 #  pt : the permutation table
 #  border_size : the size of a side of your canvas, assuming it's a square
-def noise_2d(x: int | float, y: int | float, pt: list[int], border_size: int) -> float:
+def _noise_2d(x: int | float, y: int | float, pt: list[int], border_size: int) -> float:
     gX = math.floor(x) % border_size
     gY = math.floor(y) % border_size
     xf = x - math.floor(x)
@@ -80,97 +64,126 @@ def noise_2d(x: int | float, y: int | float, pt: list[int], border_size: int) ->
     bottom_right = pt[pt[gX + 1] + gY]
 
     # dot product of the corner point value in the permutation table as a constant vector and corner vector
-    dot_top_left = dot_product_2d(get_constant_vect(top_left), top_left_vect)
-    dot_top_right = dot_product_2d(get_constant_vect(top_right), top_right_vect)
-    dot_bottom_left = dot_product_2d(get_constant_vect(bottom_left), bottom_left_vect)
-    dot_bottom_right = dot_product_2d(get_constant_vect(bottom_right), bottom_right_vect)
+    dot_top_left = _dot_product_2d(_get_constant_vect(top_left), top_left_vect)
+    dot_top_right = _dot_product_2d(_get_constant_vect(top_right), top_right_vect)
+    dot_bottom_left = _dot_product_2d(_get_constant_vect(bottom_left), bottom_left_vect)
+    dot_bottom_right = _dot_product_2d(_get_constant_vect(bottom_right), bottom_right_vect)
 
-    u = fade(xf)
-    v = fade(yf)
+    u = _fade(xf)
+    v = _fade(yf)
 
-    n = linear_interpolation(u,
-                             linear_interpolation(v, dot_bottom_left, dot_top_left),
-                             linear_interpolation(v, dot_bottom_right, dot_top_right)
-                             )
+    n = _linear_interpolation(u,
+                              _linear_interpolation(v, dot_bottom_left, dot_top_left),
+                              _linear_interpolation(v, dot_bottom_right, dot_top_right)
+                              )
 
     return n
 
 
-#  Changes the color of every pixel of the canvas for a gray shade
-#  octave : higher value increases level of details
-def apply_noise(canvas: Image, octave: int) -> Image:
-    image = canvas.load()
-    #  Assuming the canvas is a square
-    border_size: int = canvas.height
-    #  Permutation table
-    pt = perm_table(size=border_size)
+class Noise:
 
-    for y in range(canvas.height):
-        for x in range(canvas.width):
-            coord = (int(x), int(y))
+    def __init__(self):
+        self.width = 100
+        self.height = 100
+        self.seed = None
+        self.register_path = None
+        self.image = None
+        self.octave = 1
 
-            n: float = 0.0
+    # Defines the size of the image (width, height) in pixels
+    def set_size(self, size: tuple[int, int]):
+        self.width, self.height = size
 
-            frequency: float = 0.025
-            motion: float = 1.0
+    # Returns the size of the image (width, height) in pixels
+    def get_size(self):
+        return self.width, self.height
 
-            for _ in range(octave):
-                n += motion * noise_2d(x * frequency, y * frequency, pt=pt, border_size=border_size)
-                motion *= 0.5
-                frequency *= 2
+    # Defines the seed of the random used to generate the image (positive integer only)
+    def set_seed(self, seed: int):
+        self.seed = seed
 
-            n += 1
-            n *= 0.5
-            n = round(n * 255)
+    # Returns the seed of the random used to generate the image
+    def get_seed(self):
+        return self.seed
 
-            if n < 0.5:
-                color = (0, 0, 2 * n)
-            elif n < 0.9:
-                color = (0, n, round(n * 0.5))
-            else:
-                color = (n, n, n)
+    # Defines the path to the place where to save the generated image (string)
+    def set_path(self, path: str):
+        self.register_path = path
 
-            image[coord] = color
+    # Returns the path to the place where to save the generated image
+    def get_path(self):
+        return self.register_path
 
-    return canvas
+    # Defines the number of octave used to generate the image (Positive integer only)
+    def set_octave(self, octave: int):
+        self.octave = octave
 
+    # Returns the number of octave used to generate the image (a positive integer)
+    def get_octave(self):
+        return self.octave
 
-#  Create an image of a given size and applies noise on it following octave
-def create_noise(width: int, height: int, octave: int) -> Image:
-    return apply_noise(create_canvas(width, height), octave=octave)
+    #  Returns a blank canvas following the inputted width and height
+    def create_canvas(self) -> Image:
+        self.image = Image.new("RGB", (self.width, self.height), "#FFFFFF")
 
+    #  Returns a permutation table of 2 times the inputted size
+    def perm_table(self, size: int) -> list[int]:
+        # Seed the randomness if one is specified
+        if self.seed is not None:
+            random.seed(self.seed)
 
-#  Save the given image as a png following file path and name
-def save_image(image: Image, path: str, name: str) -> Image:
-    image.save(f"{path}/{name}.png")
+        array = [i for i in range(size)]
+        random.shuffle(array)
 
+        for i in range(size):
+            array.append(array[i])
 
-#  Displays the given image
-def show_map(canvas: Image) -> None:
-    canvas.show()
+        return array
 
+    #  Changes the color of every pixel of the canvas for a gray shade
+    #  octave : higher value increases level of details
+    def apply_noise(self) -> Image:
+        image = self.image.load()
+        #  Assuming the canvas is a square
+        border_size: int = self.height
+        #  Permutation table
+        pt = self.perm_table(size=border_size)
 
-#  Practical function that creates, apply noise and displays an image
-#  User can choose to save/ preview it or not with a given path/ name (or default one)
-def render_canvas(width: int, height: int, octave: int,
-                  preview: bool = True, save: bool = False,
-                  path: str = "C:", name: str = "noise") -> None:
-    image = create_noise(width, height, octave=octave)
+        for y in range(self.height):
+            for x in range(self.width):
+                coord = (int(x), int(y))
 
-    #  User wants to preview the generated image
-    if preview:
-        show_map(image)
+                n: float = 0.0
 
-    #  User wants to save the generated image
-    if save:
-        save_name: str = name
+                frequency: float = 0.025
+                motion: float = 1.0
 
-        #  Preventing duplicate name is no specific name is given
-        if save_name == "noise":
-            save_name: str = f"noise_{(random.randint(10000, 99999))}"
+                for _ in range(self.octave):
+                    n += motion * _noise_2d(x * frequency, y * frequency, pt=pt, border_size=border_size)
+                    motion *= 0.5
+                    frequency *= 2
 
-        save_image(image, path, save_name)
+                n += 1
+                n *= 0.5
+                n = round(n * 255)
 
+                if n < 0.5:
+                    color = (0, 0, 2 * n)
+                elif n < 0.9:
+                    color = (0, n, round(n * 0.5))
+                else:
+                    color = (n, n, n)
 
-#  Example of a canvas of 256x256 with 10 octaves of Perlin noise
-render_canvas(256, 256, 10, preview=False, save=True, path="C:/Users/GabHas/Desktop/")
+                image[coord] = color
+
+    # Saves the given image as a png following file path and name
+    def save_img(self, name: str) -> Image:
+        print(type(self.image))
+        if self.register_path is not None:
+            self.image.save(f"{self.register_path}/{name}.png")
+        else:
+            raise AttributeError("Not specified path.")
+
+    # Displays the image
+    def show_img(self) -> None:
+        self.image.show()
